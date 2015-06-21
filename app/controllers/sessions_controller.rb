45247cbcf63b2613bbc1e3eb6023e6efe48fac4b
Geovanna RC <gechro@outlook.com>
2015-06-21 14:34:06 -0400

    add user login/signup plus sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,23 @@
 class SessionsController < ApplicationController
 
+  def create
+    @user= User.find_by(email: user_params[:email])
+    if @user != nil
+      session[:user_id] = @user.id
+    else
+      redirect_to create_session_path
+    end
+  end
+
+  def destroy
+      session[:user_id] = nil
+      redirect_to create_session_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
