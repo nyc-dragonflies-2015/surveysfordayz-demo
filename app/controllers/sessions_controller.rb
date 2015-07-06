6f36cb18d1f1a4a31eca78b62024083a3ed624e6
Steven G. Harms <steven@devbootcamp.com>
2015-07-06 16:40:44 -0400

    Sessions#create: Use authenticate to validate pw

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: user_params[:email])
-    if @user != nil
+    if @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
       redirect_to @user
     else
