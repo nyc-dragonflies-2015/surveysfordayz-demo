5bb12970badebdad07d332a99e283c2c3fdc4679
Steven G. Harms <steven@devbootcamp.com>
2015-07-06 16:42:41 -0400

    SurveyCreateSpec: Refactor to use before(:each)

--- spec/features/survey_create_spec.rb
+++ spec/features/survey_create_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 feature 'User signs in' do
   given!(:user) { User.create(email: "mrpants666@example.com", password: "DevilPants") }
 
+  before(:each) do
+    visit root_path
+    fill_in 'Email', with: user.email
+  end
+
   context "When the credentials are good" do
     it "lets a user sign in" do
-      visit root_path
-      fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
       click_button 'Login'
       friendly_string = "Hello #{user.email}!"
@@ -16,8 +19,6 @@ feature 'User signs in' do
 
   context "When the password is bad" do
     it "does not let a user without a valid password sign in" do
-      visit root_path
-      fill_in 'Email', with: user.email
       fill_in 'Password', with: "I love jellybeans!"
       click_button 'Login'
       expect(page).to have_content "die in a fire!"
