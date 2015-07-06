38dd5e69f0b2fdc3c253a8a9fc64e544cdf52d39
Steven G. Harms <steven@devbootcamp.com>
2015-07-06 16:37:52 -0400

    Sessions#create: Implement failed login with flash msg

--- spec/features/survey_create_spec.rb
+++ spec/features/survey_create_spec.rb
@@ -11,4 +11,16 @@ feature 'User signs in' do
     friendly_string = "Hello #{user.email}!"
     expect(page).to have_content friendly_string
   end
+
+end
+
+feature "Logging in with bad credentials says bad things" do
+  given!(:user) { User.create(email: "mrpants666@example.com", password: "DevilPants") }
+  it "does not let a user without a valid password sign in" do
+    visit root_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: "I love jellybeans!"
+    click_button 'Login'
+    expect(page).to have_content "die in a fire!"
+  end
 end
