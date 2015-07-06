7627ce49a36dad460fadc6bfd80153eec89d3ed2
Steven G. Harms <steven@devbootcamp.com>
2015-07-06 16:33:22 -0400

    SessionsController#create: Add controller test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,4 +7,18 @@ describe SessionsController do
       expect(response).to render_template("new")
     end
   end
+
+  describe "POST /sessions" do
+    let!(:user) { User.create :email => "natas@hairybug.com", :password => "love" }
+
+    it "sign me in with the correct credentials" do
+      post :create, user: { email: user.email, password: user.password }
+      expect(response).to redirect_to(user)
+    end
+
+    it "NOT sign me in with the incorrect credentials" do
+      post :create, user: { email: user.email, password: "Hatred" }
+      expect(response).to redirect_to(root_path)
+    end
+  end
 end
