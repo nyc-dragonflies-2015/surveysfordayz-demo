4613aed8fc418a75011424b029ee3ec69144cc01
Steven G. Harms <steven@devbootcamp.com>
2015-07-06 16:39:47 -0400

    User#authenticate: rm method blocking BCrypt
    `authenticate` was blocking the method defined,
    for free, by BCrypt.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,4 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :taken_surveys
   has_many :surveys
-
-
-  def authenticate(password)
-     self.password == password
-  end
-
 end
