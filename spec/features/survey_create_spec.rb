d283e9bab21ceaec3ae23775e35795e77f8f89f4
Steven G. Harms <steven@devbootcamp.com>
2015-07-06 16:42:41 -0400

    SurveyCreateSpec: Refactor to use contexts in feature

--- spec/features/survey_create_spec.rb
+++ spec/features/survey_create_spec.rb
@@ -3,24 +3,24 @@ require 'rails_helper'
 feature 'User signs in' do
   given!(:user) { User.create(email: "mrpants666@example.com", password: "DevilPants") }
 
-  it "lets a user sign in" do
-    visit root_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Login'
-    friendly_string = "Hello #{user.email}!"
-    expect(page).to have_content friendly_string
+  context "When the credentials are good" do
+    it "lets a user sign in" do
+      visit root_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Login'
+      friendly_string = "Hello #{user.email}!"
+      expect(page).to have_content friendly_string
+    end
   end
 
-end
-
-feature "Logging in with bad credentials says bad things" do
-  given!(:user) { User.create(email: "mrpants666@example.com", password: "DevilPants") }
-  it "does not let a user without a valid password sign in" do
-    visit root_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: "I love jellybeans!"
-    click_button 'Login'
-    expect(page).to have_content "die in a fire!"
+  context "When the password is bad" do
+    it "does not let a user without a valid password sign in" do
+      visit root_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: "I love jellybeans!"
+      click_button 'Login'
+      expect(page).to have_content "die in a fire!"
+    end
   end
 end
